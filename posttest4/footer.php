2c592690e90c844f9c7a37f76ed7eedeab18cc05
RaflyOlanda <162123130+RaflyOlanda@users.noreply.github.com>
2024-10-02 20:28:14 +0800

    Delete posttest4 directory

--- posttest4/footer.php
+++
@@ -1,6 +0,0 @@
-<div class="footer">
-        <footer class="footer-text">
-            <p> COPYRIGHT ©2024 designed by Muhammad Rafly Pratama Olanda</p>
-            <p> Hidup seperti larry</p>
-        </footer>
-    </div>
