02e6adcaa4b8a304b3a6e70f416fd7facb4888d7
RaflyOlanda <162123130+RaflyOlanda@users.noreply.github.com>
2024-10-02 20:30:32 +0800

    Add files via upload

---
+++ posttest4/footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+        <footer class="footer-text">
+            <p> COPYRIGHT ©2024 designed by Muhammad Rafly Pratama Olanda</p>
+            <p> Hidup seperti larry</p>
+        </footer>
+    </div>
